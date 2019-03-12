dc41c32738361f8a649f8c2207c6c59da4f3077e
armia-afsharian <48348746+armia-afsharian@users.noreply.github.com>
2019-03-13 03:18:43 +0330

    start

---
+++ aa/main.js
@@ -0,0 +1,12 @@
+/*
+
+var bn = document.getElementById('btn');
+var scrl =document.getElementsByClassName('scrol');
+if (scrl) {	
+bn.addEventListener("click", display);
+
+function display() {
+scrl.className.remove=("hide");
+};
+}	
+*/
